d39d2eae6baf357fb2033306c120284972482841
alrieq <alrieq@live.com>
2023-05-18 23:07:52 +0300

    .

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ var defaultAlbumId = 'Jfni3';
 function requestAlbum() {
     let albumId = document.getElementById("albumIdField").innerText;
     console.log(albumId);
-    if(!albumId) {
+    if (!albumId) {
         albumId = defaultAlbumId;
     }
     var req = new XMLHttpRequest();
     req.onreadystatechange = function () {
         if (req.readyState == 4 && req.status == 200) {
-            processAlbumRequest(req.responseText);
+            // processAlbumRequest(req.responseText);
+            var respObj = JSON.parse(response_text);
+            let imgElem = document.createElement("img");
+            imgElem.src = respObj.data.link;
+            //imgElem.referrerpolicy="no-referrer";
+            document.body.appendChild(imgElem);
         }
         else if (req.readyState == 4 && req.status != 200) {
             console.log(req.status + " Error with the imgur API: ", req.responseText);
@@ -22,33 +27,33 @@ function requestAlbum() {
     req.send();
 }
 
-function processAlbumRequest(response_text) {
-    var respObj = JSON.parse(response_text);
-    for (item of respObj.data.slice(0, 10)){
-        console.log(item)
-        requestImage(item.id);
-    }
-}
+// function processAlbumRequest(response_text) {
+//     var respObj = JSON.parse(response_text);
+//     for (item of respObj.data.slice(0, 10)){
+//         console.log(item)
+//         requestImage(item.id);
+//     }
+// }
 
-function requestImage(imageHash) {
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            processImageRequest(req.responseText);
-        }
-        else if (req.readyState == 4 && req.status != 200) {
-            console.log("Error with the imgur API");
-        }
-    }
-    req.open("GET", "https://api.imgur.com/3/image/" + imageHash, true); // true for asynchronous     
-    req.setRequestHeader('Authorization', 'Client-ID ' + clientId);
-    req.send();
-}
+// function requestImage(imageHash) {
+//     var req = new XMLHttpRequest();
+//     req.onreadystatechange = function () {
+//         if (req.readyState == 4 && req.status == 200) {
+//             processImageRequest(req.responseText);
+//         }
+//         else if (req.readyState == 4 && req.status != 200) {
+//             console.log("Error with the imgur API");
+//         }
+//     }
+//     req.open("GET", "https://api.imgur.com/3/image/" + imageHash, true); // true for asynchronous
+//     req.setRequestHeader('Authorization', 'Client-ID ' + clientId);
+//     req.send();
+// }
 
-function processImageRequest(response_text) {
-    var respObj = JSON.parse(response_text);
-    let imgElem = document.createElement("img");
-    imgElem.src = respObj.data.link;
-    //imgElem.referrerpolicy="no-referrer";
-    document.body.appendChild(imgElem);
-}
+// function processImageRequest(response_text) {
+//     var respObj = JSON.parse(response_text);
+//     let imgElem = document.createElement("img");
+//     imgElem.src = respObj.data.link;
+//     //imgElem.referrerpolicy="no-referrer";
+//     document.body.appendChild(imgElem);
+// }
